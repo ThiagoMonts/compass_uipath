3834a87f509a9992e9428c48c100c4b5167c007b
ThiagoMonts <thiagohonorato@hotmail.com>
2023-08-30 10:27:05 -0300

    Email Automation with Studio

---
+++ 13 Email Automation with Studio/RCA_IncidentRequest/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace RCA_IncidentRequest
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
